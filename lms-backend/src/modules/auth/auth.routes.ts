11bf0c766f667ea94705374a93b181c1bd358b2e
Progga <techzu.dev6@gmail.com>
2026-02-25 17:03:17 +0600

    ../../../src/middleware/auth.middleware fix

--- lms-backend/src/modules/auth/auth.routes.ts
+++ lms-backend/src/modules/auth/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { getMeController, loginController,registerController } from "./auth.controller";
-import { authenticate } from "../../../src/middleware/auth.middleware";
-import { authMiddleware } from "../../../src/middleware/auth.middleware";
+import { authenticate } from "@middlewares/auth.middleware";
+import { authMiddleware } from "@middlewares/auth.middleware";
 const router = Router();
 
 router.post("/login", loginController);
